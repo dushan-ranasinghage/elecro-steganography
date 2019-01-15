c98b1b6c354a26db0985045688e853a76070d711
Dushan Randika <DushanR@millenniumitesp.com>
2019-01-15 11:34:19 +0530

    Created new test window

--- App.jsx
+++ App.jsx
@@ -47,38 +47,47 @@ export default class App extends Component {
           </Container>
         </Menu>
 
-        <Container text style={{ marginTop: '7em' }}>
-        <Message
-    icon='inbox'
-    header='Choose your image and hide something!'
-    content='Effective way to exchange hidden messages without raising suspicion.'
-  />
-        <Segment placeholder>
-    <Grid columns={2} stackable textAlign='center'>
-      <Divider vertical><Icon name='arrow alternate circle right'/>
-</Divider>
-
-      <Grid.Row verticalAlign='middle'>
-        <Grid.Column>
-        <Label width="4" as="label" htmlFor="file" size="big" style={{cursor: 'pointer'}}>
-              <Icon name="file" />
-              Select Image
-            </Label>
-            <input id="file" hidden type="file" onChange={this.handleChange} onClick={() => this.setState({ isHidden: true })} />
-            
-        </Grid.Column>
-
-        <Grid.Column>
-        <img src={this.state.file} height="150px"/>
-        {/* <Button primary 
-        onClick={() => this.setState({ file: null, isHidden: false })}
-        >Clear</Button> */}
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-  </Segment>
-
+        <Container text style={{ marginTop: '5em' }}>
+          <Message
+            icon='inbox'
+            header='Select your image and hide something!'
+            content='Effective way to exchange hidden messages without raising suspicion.'
+          />
+          <Segment placeholder>
+            <Grid columns={2} stackable textAlign='center'>
+              <Divider vertical><Icon name='arrow alternate circle right' />
+              </Divider>
+              <Grid.Row verticalAlign='middle'>
+                <Grid.Column>
+                  <Label width="4" as="label" htmlFor="file" size="big" style={{ cursor: 'pointer' }}>
+                    <Icon name="file" />
+                    Select Image
+                  </Label>
+                  <input id="file" hidden type="file" onChange={this.handleChange} onClick={() => this.setState({ isHidden: true })} />
+                  <br /><br />
+                  <Button primary
+                    onClick={() => this.setState({ file: null, isHidden: false })}
+                  >Clear</Button>
+                </Grid.Column>
+                <Grid.Column>
+                  <img src={this.state.file} height="150px" />
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Segment>
         </Container>
+        <Grid columns={2} stackable textAlign='center'>
+              <Grid.Row verticalAlign='middle'>
+                <Grid.Column>
+                
+                </Grid.Column>
+                <Grid.Column>
+                  <br />
+                <Button secondary id="new-window">Hide message</Button>
+                <Button secondary>Reveal message</Button>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
 
 
       </div>
